90fd10b72c2933fb434335b0bedbff921ea3395e
johnlime <thelimejuiceproductions@gmail.com>
2020-10-12 00:25:29 +0900

    som.obj generated

--- demo/cartpole.py
+++ demo/cartpole.py
@@ -6,10 +6,10 @@ env = gym.make("CartPole-v1")
 observation = env.reset()
 maxitr = 10**3
 som = KohonenSOM(total_nodes=100, node_size=2, update_iterations=maxitr)
-pose_pool = torch.empty(maxitr, 2)
 
 sampled_length = 0
 sample_iter = 50
+pose_pool = torch.empty(sample_iter * 200, 2)
 
 for __ in range(sample_iter):
     for _ in range(200):
@@ -29,5 +29,5 @@ for iteration in range(maxitr):
     som.update(pose_pool[:sampled_length], iteration)
 
 import pickle
-filehandler = open("data/som.obj", 'w')
+filehandler = open("data/som.obj", 'wb')
 pickle.dump(som, filehandler)
