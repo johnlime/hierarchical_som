a950a197d46c5b3dede06133687d71ad41a80573
johnlime <thelimejuiceproductions@gmail.com>
2020-10-22 12:16:55 +0900

    manager experimentation and todo update

--- demo/manager_cartpole.py
+++ demo/manager_cartpole.py
@@ -7,7 +7,7 @@ from model.manager_som import ManagerSOM
 
 import matplotlib.pyplot as plt
 
-manager_maxitr = 5 * 10 ** 3
+manager_maxitr = 10 ** 3
 maxtime = 10 ** 2
 gamma = 0.99
 epsilon = 0.3
@@ -16,7 +16,7 @@ cumulative_return = []
 filehandler = open("data/som.obj", 'rb')
 som = pickle.load(filehandler)
 
-manager_som = ManagerSOM(total_nodes=49, worker_som=som, additional_state_space = 2, update_iterations=manager_maxitr)
+manager_som = ManagerSOM(total_nodes=100, worker_som=som, additional_state_space = 2, update_iterations=manager_maxitr)
 env = gym.make("CartPole-v1")
 obs = env.reset()
 
@@ -25,25 +25,23 @@ for epoch in range(manager_maxitr):
 
     for t in range(0, maxtime):
 #         env.render()
-        if t % 20 == 0:
-            # create one-hot vector for winner worker som
-            current_position = torch.zeros(manager_som.state_indices + 2)
-            current_position[som.select_winner(obs[:1])] = 1
+        # create one-hot vector for winner worker som
+        current_position = torch.zeros(manager_som.state_indices + 2)
+        current_position[som.select_winner(obs[:1])] = 1
 
-            # additional state
-            current_position[-2:] = torch.tensor(obs[2:])
-            
-            # epsilon greedy
-            if random.random() > epsilon:
-                action_index = manager_som.get_action(current_position) # deterministic
-                
-            else:
-                action_index = random.randrange(10)
-                
+        # additional state
+        current_position[-2:] = torch.tensor(obs[2:])
+
+        # epsilon greedy
+        if random.random() > epsilon:
+            action_index = manager_som.get_action(current_position) # deterministic
+
+        else:
+            action_index = random.randrange(10)
 
         # Pseudo-PD control
         k_p = 1.0
-        k_d = 1.0
+        k_d = 0.05
         action = k_p * (som.w[action_index][0] - obs[0]) + k_d * (som.w[action_index][1] - obs[1])
         if (action > 0): action = 1
         else: action = 0
@@ -65,7 +63,7 @@ for epoch in range(manager_maxitr):
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             print(epoch, total_return)
-            if epoch % 99 == 0:
+            if epoch % 9 == 0:
                 cumulative_return.append(total_return)
             break
 
