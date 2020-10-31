967b5110df9a76a234405849b1b108b014e2097a
johnlime <thelimejuiceproductions@gmail.com>
2020-10-31 19:12:18 +0900

    outline for new motor cortex model

--- model/manager_som.py
+++ model/manager_som.py
@@ -13,99 +13,67 @@ class ManagerSOM (KohonenSOM):
 
     """
     Input state vector:
-    {current_state_indices, additional_state_indices, additional_states reward}
+    {current_state_indices, reward}
 
     Weight vector:
-    {current_state_indices, additional_state_indices, additional_states, return_estimates_per_action}
+    {current_state_indices, return_estimates_per_action}
     """
 
-    def __init__(self, total_nodes = None, worker_som = None, additional_state_space = 0, update_iterations = 100):
-        # array of worker_som references and their total nodes
-        # first index should be the PID action space
+    def __init__(self, total_nodes = None, state_som = None, worker_som = None, update_iterations = 100):
+        self.state_som = state_som
         self.worker_som = worker_som
-        self.state_indices = []
-        self.add_state = additional_state_space
 
-        node_size = self.worker_som[0].total_nodes + self.add_state # action and additional states
-        # calculate state space size
-        for i in range(len(self.worker_som)):
-            self.state_indices.append(self.worker_som[i].total_nodes)
-            node_size += self.state_indices[i]
+        node_size = self.state_som.total_nodes + self.worker_som.total_nodes
 
-        # self.w consists of a worker_som-sized one hot encoder and a worker_som-sized categorical distribution
+        # self.w consists of a state_som-sized one hot encoder and a worker_som-sized categorical distribution
         super().__init__(total_nodes = total_nodes, node_size = node_size)
 
     def select_winner(self, x):
-        # x must consist of a one-hot vector for current state indices, additional SOM indices and additional states
-        return torch.argmin(torch.norm(torch.sqrt((x - self.w[:,: - self.state_indices[0]]) ** 2), p=1, dim=1), dim=0)
+        # x must consist of a one-hot vector for current state indices
+        return torch.argmin(torch.norm(torch.sqrt((x - self.w[:,:self.state_som.total_nodes]) ** 2), p=1, dim=1), dim=0)
 
     def get_action(self, x):
-        return torch.argmax(self.w[self.select_winner(x)][- self.state_indices[0]:], dim=0)
+        return torch.argmax(self.w[self.select_winner(x)][-self.worker_som.total_nodes:], dim=0)
 
     def get_softmax(self, x):
         return torch.max(f.softmax(self.w[
             self.select_winner(x)
-            ][- self.state_indices[0]:]))[0]
+            ][-self.worker_som.total_nodes:]))[0]
 
     def action_q_learning(
             self,
-            current_winner_indices = None, # should be an array input
-            additional_states = None,
-            next_winner_index = None, # selected action
+            state_index = None,
+            action_index = None,
             reward = 0,
             t = None,
             htype = 0,
             lr = 0.9,
             gamma = 0.9):
-        current_winner = torch.zeros(self.node_size - self.state_indices[0])
-        prev_index = 0
-        for i in range(len(current_winner_indices)):
-            current_winner[prev_index: prev_index + self.state_indices[i]][current_winner_indices[i]] = 1
-            prev_index += self.state_indices[i]
-
-        current_state_space = None
-
-        if self.add_state != 0:
-            current_state_space = torch.cat((current_winner, torch.tensor(additional_states).float()))
-
-        else:
-            current_state_space = current_winner
+        current_state_space = torch.zeros(self.state_som.total_nodes)
+        current_state_space[state_index] = 1
 
         winner_c = self.select_winner(current_state_space)
 
         # update q-value using new reward and largest est. prob of action
-        self.w[winner_c][- self.state_indices[0] + next_winner_index] += lr * (
+        self.w[winner_c][self.state_som.total_nodes + action_index] += lr * (
             reward
             + gamma * self.get_softmax(current_state_space)
-            - self.w[winner_c][- self.state_indices[0] + next_winner_index]
+            - self.w[winner_c][self.state_som.total_nodes + action_index]
             )
 
-        part_x = torch.cat((current_state_space, self.w[winner_c][- self.state_indices[0] :]))
+        part_x = torch.cat((current_state_space, self.w[winner_c][self.worker_som.total_nodes:]))
 
         if htype==0:
-            self.w[:, : -self.state_indices[0]] += self.h0(winner_c, t) * (
+            self.w[:, :self.state_som.total_nodes] += self.h0(winner_c, t) * (
                 current_state_space
-                - self.w[:, : -self.state_indices[0]]
+                - self.w[:, :self.state_som.total_nodes]
                 )
-
-            # # reward neighboring
-            # self.w[:, next_winner_index - self.state_indices[0]] += torch.transpose(self.h0(winner_c, t), 0, 1)[0] * (
-            #     self.w[winner_c][next_winner_index - self.state_indices[0]]
-            #     - self.w[:, next_winner_index - self.state_indices[0]]
-            #     )
 
         elif htype==1:
-            self.w[:, : -self.state_indices[0]] += self.h1(winner_c, t) * (
+            self.w[:, :self.state_som.total_nodes] += self.h1(winner_c, t) * (
                 current_state_space
-                - self.w[:, : -self.state_indices[0]]
+                - self.w[:, :self.state_som.total_nodes]
                 )
-
-            # # reward neighboring
-            # self.w[:, next_winner_index - self.state_indices[0]] += torch.transpose(self.h1(winner_c, t), 0, 1)[0] * (
-            #     self.w[winner_c][next_winner_index - self.state_indices[0]]
-            #     - self.w[:, next_winner_index - self.state_indices[0]]
-            #     )
 
     def update(self, x=None, t=None, htype=0):
         raise NameError('Use action_q_learning() instead of update()')
-        return None, None
