f8d72f5b2dffb966966395d2dbdb8441337441bd
johnlime <thelimejuiceproductions@gmail.com>
2020-11-26 10:43:05 +0900

    prep for implementing FALCON

---
+++ model/falcon.py
@@ -0,0 +1,15 @@
+# import torch
+# import torch.nn.functional as f
+# import math
+# import random
+#
+# try:
+#     import kohonen_som
+# except:
+#     print("path exception")
+# finally:
+#     from model.kohonen_som import KohonenSOM
+#
+# class FALCON (KohonenSOM):
+#     def __init__ ():
+#         super().__init__(total_nodes = total_nodes, node_size = node_size)
