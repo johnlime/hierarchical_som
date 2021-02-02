bc349c83e9e4dadde14b8dad5e929f9f2abc88c4
johnlime <thelimejuiceproductions@gmail.com>
2021-02-02 11:01:43 +0900

    added pose visualizer for BipedalWalker

---
+++ custom_env/bipedal_walker_poses.py
@@ -0,0 +1,164 @@
+from custom_env.bipedal_walker_clone import (ContactDetector, BipedalWalker,
+                                            VIEWPORT_W,
+                                            VIEWPORT_H,
+                                            SCALE,
+                                            TERRAIN_STEP,
+                                            TERRAIN_STARTPAD,
+                                            TERRAIN_HEIGHT,
+                                            LEG_H,
+                                            LEG_W,
+                                            HULL_FD,
+                                            LEG_DOWN,
+                                            LEG_FD,
+                                            MOTORS_TORQUE,
+                                            LOWER_FD)
+import numpy as np
+import Box2D
+from Box2D.b2 import circleShape, revoluteJointDef
+
+
+class BipedalWalkerPoses (BipedalWalker):
+    def _generate_terrain(self, hardcore):
+        self.terrain   = []
+        self.terrain_x = []
+        self.terrain_y = []
+        self.terrain_poly = []
+
+    def _generate_clouds(self):
+        self.cloud_poly   = []
+
+    def reset(self):
+        self._destroy()
+        self.world.contactListener_bug_workaround = ContactDetector(self)
+        self.world.contactListener = self.world.contactListener_bug_workaround
+        self.game_over = False
+        self.prev_shaping = None
+        self.scroll = 0.0
+        self.lidar_render = 0
+
+        W = VIEWPORT_W/SCALE
+        H = VIEWPORT_H/SCALE
+
+        self._generate_terrain(self.hardcore)
+        self._generate_clouds()
+
+        init_x = TERRAIN_STEP*TERRAIN_STARTPAD/2
+        init_y = TERRAIN_HEIGHT+2*LEG_H
+        self.hull = self.world.CreateStaticBody( # CreateDynamicBody(
+            position = (init_x, init_y),
+            fixtures = HULL_FD
+                )
+        self.hull.color1 = (0.5,0.4,0.9)
+        self.hull.color2 = (0.3,0.3,0.5)
+        # self.hull.ApplyForceToCenter((self.np_random.uniform(-INITIAL_RANDOM, INITIAL_RANDOM), 0), True)
+
+        self.legs = []
+        self.joints = []
+        for i in [-1,+1]:
+            leg = self.world.CreateDynamicBody(
+                position = (init_x, init_y - LEG_H/2 - LEG_DOWN),
+                angle = (i*0.05),
+                fixtures = LEG_FD
+                )
+            leg.color1 = (0.6-i/10., 0.3-i/10., 0.5-i/10.)
+            leg.color2 = (0.4-i/10., 0.2-i/10., 0.3-i/10.)
+            rjd = revoluteJointDef(
+                bodyA=self.hull,
+                bodyB=leg,
+                localAnchorA=(0, LEG_DOWN),
+                localAnchorB=(0, LEG_H/2),
+                enableMotor=True,
+                enableLimit=True,
+                maxMotorTorque=MOTORS_TORQUE,
+                motorSpeed = i,
+                lowerAngle = -0.8,
+                upperAngle = 1.1,
+                )
+            self.legs.append(leg)
+            self.joints.append(self.world.CreateJoint(rjd))
+
+            lower = self.world.CreateDynamicBody(
+                position = (init_x, init_y - LEG_H*3/2 - LEG_DOWN),
+                angle = (i*0.05),
+                fixtures = LOWER_FD
+                )
+            lower.color1 = (0.6-i/10., 0.3-i/10., 0.5-i/10.)
+            lower.color2 = (0.4-i/10., 0.2-i/10., 0.3-i/10.)
+            rjd = revoluteJointDef(
+                bodyA=leg,
+                bodyB=lower,
+                localAnchorA=(0, -LEG_H/2),
+                localAnchorB=(0, LEG_H/2),
+                enableMotor=True,
+                enableLimit=True,
+                maxMotorTorque=MOTORS_TORQUE,
+                motorSpeed = 1,
+                lowerAngle = -1.6,
+                upperAngle = -0.1,
+                )
+            lower.ground_contact = False
+            self.legs.append(lower)
+            self.joints.append(self.world.CreateJoint(rjd))
+
+        self.drawlist = self.terrain + self.legs + [self.hull]
+
+        class LidarCallback(Box2D.b2.rayCastCallback):
+            def ReportFixture(self, fixture, point, normal, fraction):
+                if (fixture.filterData.categoryBits & 1) == 0:
+                    return -1
+                self.p2 = point
+                self.fraction = fraction
+                return fraction
+        self.lidar = [LidarCallback() for _ in range(10)]
+
+        return self.step(np.array([0,0,0,0]))[0]
+
+    def render(self, mode='human'):
+        from gym.envs.classic_control import rendering
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(VIEWPORT_W, VIEWPORT_H)
+        self.viewer.set_bounds(self.scroll, VIEWPORT_W/SCALE + self.scroll, 0, VIEWPORT_H/SCALE)
+
+        self.viewer.draw_polygon( [
+            (self.scroll,                  0),
+            (self.scroll+VIEWPORT_W/SCALE, 0),
+            (self.scroll+VIEWPORT_W/SCALE, VIEWPORT_H/SCALE),
+            (self.scroll,                  VIEWPORT_H/SCALE),
+            ], color=(0.9, 0.9, 1.0) )
+        for poly,x1,x2 in self.cloud_poly:
+            if x2 < self.scroll/2: continue
+            if x1 > self.scroll/2 + VIEWPORT_W/SCALE: continue
+            self.viewer.draw_polygon( [(p[0]+self.scroll/2, p[1]) for p in poly], color=(1,1,1))
+        for poly, color in self.terrain_poly:
+            if poly[1][0] < self.scroll: continue
+            if poly[0][0] > self.scroll + VIEWPORT_W/SCALE: continue
+            self.viewer.draw_polygon(poly, color=color)
+
+        self.lidar_render = (self.lidar_render+1) % 100
+        i = self.lidar_render
+        if i < 2*len(self.lidar):
+            l = self.lidar[i] if i < len(self.lidar) else self.lidar[len(self.lidar)-i-1]
+            self.viewer.draw_polyline( [l.p1, l.p2], color=(1,0,0), linewidth=1 )
+
+        for obj in self.drawlist:
+            for f in obj.fixtures:
+                trans = f.body.transform
+                if type(f.shape) is circleShape:
+                    t = rendering.Transform(translation=trans*f.shape.pos)
+                    self.viewer.draw_circle(f.shape.radius, 30, color=obj.color1).add_attr(t)
+                    self.viewer.draw_circle(f.shape.radius, 30, color=obj.color2, filled=False, linewidth=2).add_attr(t)
+                else:
+                    path = [trans*v for v in f.shape.vertices]
+                    self.viewer.draw_polygon(path, color=obj.color1)
+                    path.append(path[0])
+                    self.viewer.draw_polyline(path, color=obj.color2, linewidth=2)
+
+        # flagy1 = TERRAIN_HEIGHT
+        # flagy2 = flagy1 + 50/SCALE
+        # x = TERRAIN_STEP*3
+        # self.viewer.draw_polyline( [(x, flagy1), (x, flagy2)], color=(0,0,0), linewidth=2 )
+        # f = [(x, flagy2), (x, flagy2-10/SCALE), (x+25/SCALE, flagy2-5/SCALE)]
+        # self.viewer.draw_polygon(f, color=(0.9,0.2,0) )
+        # self.viewer.draw_polyline(f + [f[0]], color=(0,0,0), linewidth=2 )
+
+        return self.viewer.render(return_rgb_array = mode=='rgb_array')
