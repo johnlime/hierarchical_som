a2ec5c4225a05bccccc89ecb37a9152f4d42efe2
Mojave Lime <mojavelime@Mojaves-MacBook-Pro.local>
2021-02-28 14:18:27 +0900

    added pose obtaining environment for bipedal walker

--- custom_env/bipedal_walker_poses.py
+++ custom_env/bipedal_walker_poses.py
@@ -15,9 +15,14 @@ from custom_env.bipedal_walker_clone import (ContactDetector, BipedalWalker,
 import numpy as np
 import Box2D
 from Box2D.b2 import circleShape, revoluteJointDef
+import random
 
 
 class BipedalWalkerPoses (BipedalWalker):
+    def __init__(self):
+        self.target_pose = 4 * [0.0] # array depicting pose; initialized
+        super().__init__()
+
     def _generate_terrain(self, hardcore):
         self.terrain   = []
         self.terrain_x = []
@@ -26,6 +31,10 @@ class BipedalWalkerPoses (BipedalWalker):
 
     def _generate_clouds(self):
         self.cloud_poly   = []
+
+    def _generate_target_pose(self):
+        for i in range(0, 4):
+            self.target_pose[i] = 2.0 * random.random() - 1.0
 
     def reset(self):
         self._destroy()
@@ -41,6 +50,7 @@ class BipedalWalkerPoses (BipedalWalker):
 
         self._generate_terrain(self.hardcore)
         self._generate_clouds()
+        self._generate_target_pose()
 
         init_x = TERRAIN_STEP*TERRAIN_STARTPAD/2
         init_y = TERRAIN_HEIGHT+2*LEG_H
@@ -112,6 +122,80 @@ class BipedalWalkerPoses (BipedalWalker):
         self.lidar = [LidarCallback() for _ in range(10)]
 
         return self.step(np.array([0,0,0,0]))[0]
+
+    def step(self, action):
+        #self.hull.ApplyForceToCenter((0, 20), True) -- Uncomment this to receive a bit of stability help
+        control_speed = False  # Should be easier as well
+        if control_speed:
+            self.joints[0].motorSpeed = float(SPEED_HIP  * np.clip(action[0], -1, 1))
+            self.joints[1].motorSpeed = float(SPEED_KNEE * np.clip(action[1], -1, 1))
+            self.joints[2].motorSpeed = float(SPEED_HIP  * np.clip(action[2], -1, 1))
+            self.joints[3].motorSpeed = float(SPEED_KNEE * np.clip(action[3], -1, 1))
+        else:
+            self.joints[0].motorSpeed     = float(SPEED_HIP     * np.sign(action[0]))
+            self.joints[0].maxMotorTorque = float(MOTORS_TORQUE * np.clip(np.abs(action[0]), 0, 1))
+            self.joints[1].motorSpeed     = float(SPEED_KNEE    * np.sign(action[1]))
+            self.joints[1].maxMotorTorque = float(MOTORS_TORQUE * np.clip(np.abs(action[1]), 0, 1))
+            self.joints[2].motorSpeed     = float(SPEED_HIP     * np.sign(action[2]))
+            self.joints[2].maxMotorTorque = float(MOTORS_TORQUE * np.clip(np.abs(action[2]), 0, 1))
+            self.joints[3].motorSpeed     = float(SPEED_KNEE    * np.sign(action[3]))
+            self.joints[3].maxMotorTorque = float(MOTORS_TORQUE * np.clip(np.abs(action[3]), 0, 1))
+
+        self.world.Step(1.0/FPS, 6*30, 2*30)
+
+        pos = self.hull.position
+        vel = self.hull.linearVelocity
+
+        for i in range(10):
+            self.lidar[i].fraction = 1.0
+            self.lidar[i].p1 = pos
+            self.lidar[i].p2 = (
+                pos[0] + math.sin(1.5*i/10.0)*LIDAR_RANGE,
+                pos[1] - math.cos(1.5*i/10.0)*LIDAR_RANGE)
+            self.world.RayCast(self.lidar[i], self.lidar[i].p1, self.lidar[i].p2)
+
+        state = [
+            self.hull.angle,        # Normal angles up to 0.5 here, but sure more is possible.
+            2.0*self.hull.angularVelocity/FPS,
+            0.3*vel.x*(VIEWPORT_W/SCALE)/FPS,  # Normalized to get -1..1 range
+            0.3*vel.y*(VIEWPORT_H/SCALE)/FPS,
+            self.joints[0].angle,   # This will give 1.1 on high up, but it's still OK (and there should be spikes on hiting the ground, that's normal too)
+            self.joints[0].speed / SPEED_HIP,
+            self.joints[1].angle + 1.0,
+            self.joints[1].speed / SPEED_KNEE,
+            1.0 if self.legs[1].ground_contact else 0.0,
+            self.joints[2].angle,
+            self.joints[2].speed / SPEED_HIP,
+            self.joints[3].angle + 1.0,
+            self.joints[3].speed / SPEED_KNEE,
+            1.0 if self.legs[3].ground_contact else 0.0
+            ]
+        state += [l.fraction for l in self.lidar]
+        assert len(state)==24
+
+        self.scroll = pos.x - VIEWPORT_W/SCALE/5
+
+        shaping  = 130*pos[0]/SCALE   # moving forward is a way to receive reward (normalized to get 300 on completion)
+        shaping -= 5.0*abs(state[0])  # keep head straight, other than that and falling, any behavior is unpunished
+
+        reward = 0
+        for i in range(4):
+            reward += abs(self.joints[i] - self.target_pose[i])
+        # if self.prev_shaping is not None:
+        #     reward = shaping - self.prev_shaping
+        # self.prev_shaping = shaping
+
+        for a in action:
+            reward -= 0.00035 * MOTORS_TORQUE * np.clip(np.abs(a), 0, 1)
+            # normalized to about -50.0 using heuristic, more optimal agent should spend less
+
+        done = False
+        if self.game_over or pos[0] < 0:
+            reward = -100
+            done   = True
+        if pos[0] > (TERRAIN_LENGTH-TERRAIN_GRASS)*TERRAIN_STEP:
+            done   = True
+        return np.array(state), reward, done, {}
 
     def render(self, mode='human'):
         from gym.envs.classic_control import rendering
