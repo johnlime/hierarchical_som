e6948e7c5981403638ec41939a6f042e867ccbb8
Mojave Lime <mojavelime@Mojaves-MacBook-Pro.local>
2021-03-17 12:43:12 +0900

    added NavigationTaskDirectional

--- custom_env/navigation_task.py
+++ custom_env/navigation_task.py
@@ -18,7 +18,7 @@ class NavigationTask():
         step_radian = math.asin(step_vector[1]) # positive or negative radian
         if math.acos(step_vector[0]) < 0:
             step_radian = step_radian / abs(step_radian) * math.pi - step_radian
-            
+
         optimal_radian = math.asin(optimal_vector[1])
         if math.acos(optimal_vector[0]) < 0:
             optimal_radian = optimal_radian / abs(optimal_radian) * math.pi - optimal_radian
@@ -27,10 +27,30 @@ class NavigationTask():
         if reward > math.pi:
             reward = 2 * math.pi - reward
         reward = math.pi / 4 - reward
-        
+
         self.current_position += self.speed * step_vector
 
         return reward, self.current_position
 
     def state(self):
         return self.current_position
+
+class NavigationTaskDirectional():
+    def step(self, step_radian):
+        optimal_vector = (self.goal - self.current_position) / torch.sqrt(torch.sum((self.goal - self.current_position) ** 2))
+
+        if step_radian >= 2.0 * math.pi or step_radian < 0:
+            raise Exception("step_radian should be 0 <= x < 2pi")
+
+        optimal_radian = math.asin(optimal_vector[1])
+        if math.acos(optimal_vector[0]) < 0:
+            optimal_radian = optimal_radian / abs(optimal_radian) * math.pi - optimal_radian
+
+        reward = abs(step_radian - optimal_radian)
+        if reward > math.pi:
+            reward = 2 * math.pi - reward
+        reward = math.pi / 4 - reward
+
+        self.current_position += self.speed * step_vector
+
+        return reward, self.current_position
